3e5e6120e60f7b9d7f42ce793e054f2bc3e6184a
swaseem@creativechaos.co <mz7ncgZDJqPzBLNiRnxj>
2024-10-30 14:04:07 +0500

    Revert "Merge branch 'main' into firebase-integration-init"
    This reverts commit 487431daafb1c862f642f8bd035c5692f5384f55, reversing
    changes made to fd8fc1762312e8b61a74cf0c97f1050676752520.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,48 +1,9 @@
 import { Stack } from "expo-router";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
-import {
-  BottomSheetModal,
-  BottomSheetView,
-  BottomSheetModalProvider,
-} from "@gorhom/bottom-sheet";
+
 export default function RootLayout() {
   return (
-    <>
-      <GestureHandlerRootView style={{ flex: 1 }}>
-        <BottomSheetModalProvider>
-          <Stack screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="(auth)/Welcome" />
-            <Stack.Screen name="(auth)/Login" />
-            <Stack.Screen name="(auth)/Register" />
-            <Stack.Screen
-              name="(auth)/Verification"
-              options={{
-                headerShown: true,
-                headerTitle: "Phone Verification",
-                headerTitleStyle: { color: "#fff" },
-                headerTitleAlign: "center",
-                headerStyle: { backgroundColor: "#000" },
-                headerBackTitleVisible: false,
-                headerBackButtonMenuEnabled: false,
-                headerTintColor: "#fff",
-              }}
-            />
-            <Stack.Screen
-              name="(auth)/SetPassword"
-              options={{
-                headerShown: true,
-                headerTitle: "Set Password",
-                headerTitleStyle: { color: "#fff" },
-                headerStyle: { backgroundColor: "#000" },
-                headerBackTitleVisible: false,
-                headerBackButtonMenuEnabled: false,
-                headerTintColor: "#fff",
-              }}
-            />
-            <Stack.Screen name="(bottomTabs)" />
-          </Stack>
-        </BottomSheetModalProvider>
-      </GestureHandlerRootView>
-    </>
+    <Stack>
+      <Stack.Screen name="index" />
+    </Stack>
   );
 }
