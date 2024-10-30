4c4fe277f639795a3f038f7ff2f2a981c11ee0fe
swaseem@creativechaos.co <mz7ncgZDJqPzBLNiRnxj>
2024-10-30 14:22:52 +0500

    libs added

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/(auth)/Login` | `/(auth)/Register` | `/(auth)/SetPassword` | `/(auth)/Verification` | `/(auth)/Welcome` | `/(bottomTabs)` | `/(bottomTabs)/Chat` | `/(bottomTabs)/Home` | `/(bottomTabs)/Likes` | `/(bottomTabs)/Maps` | `/(bottomTabs)/Search` | `/Chat` | `/DummyScreen` | `/Home` | `/Likes` | `/Login` | `/Maps` | `/Register` | `/Search` | `/SetPassword` | `/Verification` | `/Welcome` | `/_sitemap`;
+      StaticRoutes: `/` | `/_sitemap`;
       DynamicRoutes: never;
       DynamicRouteTemplate: never;
     }
