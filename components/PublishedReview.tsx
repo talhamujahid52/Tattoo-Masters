dceddbb3fbaffc9b081850c760e64a84b9666dd0
Talha <talhamujahid52@gmail.com>
2025-04-20 12:39:22 +0500

    Report Review

--- components/PublishedReview.tsx
+++ components/PublishedReview.tsx
@@ -2,6 +2,8 @@ import { StyleSheet, TouchableOpacity, View, Image } from "react-native";
 import React from "react";
 import Text from "./Text";
 import { formatDistanceToNow } from "date-fns"; // Import date-fns
+import useBottomSheet from "@/hooks/useBottomSheet";
+import ReportBottomSheet from "@/components/BottomSheets/ReportBottomSheet";
 
 interface Review {
   artistId: string;
@@ -18,94 +20,125 @@ interface Review {
   imageUrl: string;
 }
 
-const PublishedReview = ({ review }: { review: Review }) => {
-  // Convert the review date to a JavaScript Date object
-  const reviewDate = new Date(review.date.seconds * 1000);
+const options = [
+  { label: "Nudity or sexual content", value: "Nudity or sexual content" },
+  { label: "Inappropriate content", value: "Inappropriate content" },
+  { label: "False review", value: "False review" },
+  { label: "Other", value: "Other" },
+];
 
-  // Get the time difference in a human-readable format
+const PublishedReview = ({ review }: { review: Review }) => {
+  const reviewDate = new Date(review.date.seconds * 1000);
   const timeAgo = formatDistanceToNow(reviewDate, { addSuffix: true });
 
+  const {
+    BottomSheet: ReportSheet,
+    show: showReportSheet,
+    hide: hideReportSheet,
+  } = useBottomSheet();
+
   return (
-    <View style={styles.container}>
-      <View style={styles.topRow}>
-        <View style={styles.profilePictureAndName}>
-          <Image
-            style={styles.profilePicture}
-            source={
-              review?.userProfilePicture
-                ? { uri: review?.userProfilePicture }
-                : require("../assets/images/Artist.png")
-            }
+    <>
+      <ReportSheet
+        InsideComponent={
+          <ReportBottomSheet
+            hideReportSheet={hideReportSheet}
+            title="Review"
+            options={options}
+            reportItem={review?.id}
           />
-          <View
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "space-around",
+        }
+      />
+      <View style={styles.container}>
+        <View style={styles.topRow}>
+          <View style={styles.profilePictureAndName}>
+            <Image
+              style={styles.profilePicture}
+              source={
+                review?.userProfilePicture
+                  ? { uri: review?.userProfilePicture }
+                  : require("../assets/images/Artist.png")
+              }
+            />
+            <View
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-around",
+              }}
+            >
+              <Text size="p" weight="normal" color="#FFF">
+                {review?.userName ? review?.userName : "Martin Luis"}
+              </Text>
+              <Text size="medium" weight="normal" color="#A7A7A7">
+                {timeAgo} {/* Display the calculated time ago */}
+              </Text>
+            </View>
+            <View style={styles.midRow}>
+              <Image
+                style={styles.icon}
+                source={require("../assets/images/star.png")}
+              />
+              <Text size="p" weight="normal" color="#FBF6FA">
+                {review?.rating ? review?.rating : "4.5"}
+              </Text>
+            </View>
+          </View>
+
+          <TouchableOpacity
+            onPress={() => {
+              showReportSheet();
             }}
           >
-            <Text size="p" weight="normal" color="#FFF">
-              {review?.userName ? review?.userName : "Martin Luis"}
-            </Text>
-            <Text size="medium" weight="normal" color="#A7A7A7">
-              {timeAgo} {/* Display the calculated time ago */}
-            </Text>
-          </View>
-          <View style={styles.midRow}>
             <Image
               style={styles.icon}
-              source={require("../assets/images/star.png")}
+              source={require("../assets/images/report-flag.png")}
             />
-            <Text size="p" weight="normal" color="#FBF6FA">
-              {review?.rating ? review?.rating : "4.5"}
-            </Text>
-          </View>
+          </TouchableOpacity>
         </View>
 
-        <TouchableOpacity onPress={() => {}}>
-          <Image
-            style={styles.icon}
-            source={require("../assets/images/report-flag.png")}
-          />
-        </TouchableOpacity>
-      </View>
-
-      <View
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-between",
-          marginTop: 16,
-        }}
-      >
-        <Text size="p" weight="normal" color="#A7A7A7" style={{ width: "70%" }}>
-          {review?.feedback
-            ? review?.feedback
-            : "This is the 3rd review. This artist is going great."}
-        </Text>
         <View
           style={{
-            height: 90,
-            width: "27%",
-            borderRadius: 8,
-            overflow: "hidden",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            marginTop: 16,
           }}
         >
-          <Image
+          <Text
+            size="p"
+            weight="normal"
+            color="#A7A7A7"
+            style={{ width: "70%" }}
+          >
+            {review?.feedback
+              ? review?.feedback
+              : "This is the 3rd review. This artist is going great."}
+          </Text>
+          <View
             style={{
-              height: "100%",
-              width: "100%",
-              resizeMode: "cover",
+              height: 90,
+              width: "27%",
+              borderRadius: 8,
+              overflow: "hidden",
             }}
-            source={
-              review.imageUrl
-                ? { uri: review.imageUrl }
-                : require("../assets/images/Artist.png")
-            }
-          />
+          >
+            <Image
+              style={{
+                height: "100%",
+                width: "100%",
+                resizeMode: "cover",
+              }}
+              source={
+                review.imageUrl
+                  ? { uri: review.imageUrl }
+                  : require("../assets/images/Artist.png")
+              }
+            />
+          </View>
         </View>
       </View>
-    </View>
+    </>
   );
 };
 
